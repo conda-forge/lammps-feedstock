ef4fbb47faaa35fbdacd61b4e3fde71206f2ebe5
Jan Janßen <jan-janssen@users.noreply.github.com>
2018-05-15 11:45:21 +0200

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,23 +2,15 @@
 
 args="-DENABLE_ASPHERE=ON -DENABLE_BODY=ON -DENABLE_CLASS2=ON -DENABLE_COLLOID=ON -DENABLE_COMPRESS=OFF -DENABLE_CORESHELL=ON -DENABLE_DIPOLE=ON -DENABLE_GRANULAR=ON -DENABLE_KSPACE=ON -DENABLE_MANYBODY=ON -DENABLE_MC=ON -DENABLE_MEAM=ON -DENABLE_MISC=ON -DENABLE_MOLECULE=ON -DENABLE_PERI=ON -DENABLE_REAX=ON -DENABLE_REPLICA=ON -DENABLE_RIGID=ON -DENABLE_SHOCK=ON -DENABLE_SNAP=ON -DENABLE_SRD=ON -DENABLE_OPT=ON -DENABLE_KIM=OFF -DENABLE_GPU=OFF -DENABLE_KOKKOS=OFF -DENABLE_MPIIO=OFF -DENABLE_MSCG=OFF -DENABLE_VORONOI=OFF -DENABLE_LATTE=OFF -DENABLE_PYTHON=OFF -DENABLE_USER-MEAMC=ON -DENABLE_USER-PHONON=ON -DENABLE_GZIP=ON"
 
-# MPI
-cmake -DBUILD_MPI=ON ./cmake $args  
+# Serial 
+cmake ./cmake $args  
 make
-cp lmp $PREFIX/bin/lmp_mpi
-rm CMakeCache.txt 
-
-# serial 
-cmake ./cmake $args 
-make 
-cp lmp $PREFIX/bin/lmp_serial
-rm CMakeCache.txt
+cp lmp $PREFIX/bin/lmp
 
 # Library
 cmake -DBUILD_SHARED_LIBS=ON ./cmake $args  
 make
+cp liblammps.so src  # For compatibility with the original make system.
 cd ./python
-python install.py # the installation currently fails because the library file liblammps.so does not exist!
+python install.py 
 cd ..
-cp liblammps.so ${SP_DIR} 
-rm CMakeCache.txt
