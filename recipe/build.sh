a9ad68932a30e0e0f1a05503818fbf0849ceb017
Jan Janßen <jan-janssen@users.noreply.github.com>
2018-05-15 10:30:19 +0200

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,19 +3,19 @@
 args="-DENABLE_ASPHERE=ON -DENABLE_BODY=ON -DENABLE_CLASS2=ON -DENABLE_COLLOID=ON -DENABLE_COMPRESS=OFF -DENABLE_CORESHELL=ON -DENABLE_DIPOLE=ON -DENABLE_GRANULAR=ON -DENABLE_KSPACE=ON -DENABLE_MANYBODY=ON -DENABLE_MC=ON -DENABLE_MEAM=ON -DENABLE_MISC=ON -DENABLE_MOLECULE=ON -DENABLE_PERI=ON -DENABLE_REAX=ON -DENABLE_REPLICA=ON -DENABLE_RIGID=ON -DENABLE_SHOCK=ON -DENABLE_SNAP=ON -DENABLE_SRD=ON -DENABLE_OPT=ON -DENABLE_KIM=OFF -DENABLE_GPU=OFF -DENABLE_KOKKOS=OFF -DENABLE_MPIIO=OFF -DENABLE_MSCG=OFF -DENABLE_VORONOI=OFF -DENABLE_LATTE=OFF -DENABLE_PYTHON=OFF -DENABLE_USER-MEAMC=ON -DENABLE_USER-PHONON=ON -DENABLE_GZIP=ON"
 
 # MPI
-cmake "$args -DBUILD_MPI=ON" ./cmake 
+cmake ./cmake "$args -DBUILD_MPI=ON"  
 make
 cp lmp $PREFIX/bin/lmp_mpi
 
 # serial 
-cmake $args ./cmake 
+cmake ./cmake $args 
 make 
 cp lmp $PREFIX/bin/lmp_serial
 
 # Library
-cmake "$args -DBUILD_SHARED_LIBS=ON" ./cmake 
+cmake ./cmake "$args -DBUILD_SHARED_LIBS=ON" 
 make
 cd ./python
-python install.py
+python install.py # the installation currently fails because the library file liblammps.so does not exist!
 cd ..
-cp liblammps.so ${SP_DIR} 
+# cp liblammps.so ${SP_DIR} 
