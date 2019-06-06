08a23ac691e58c9bffba7be68115b58d451da73e
Jan Janssen <jan-janssen@users.noreply.github.com>
2019-06-06 11:08:47 +0200

    Debugging Mac OS X

--- recipe/build.sh
+++ recipe/build.sh
@@ -16,6 +16,7 @@ mkdir build_lib
 cd build_lib
 cmake -D BUILD_LIB=ON -D BUILD_SHARED_LIBS=ON -D BUILD_MPI=OFF -D BUILD_OMP=OFF $args ../cmake
 make # -j${NUM_CPUS}
+ls liblammps.*
 cp liblammps.* ../src  # For compatibility with the original make system.
 cd ../src
 make install-python 
