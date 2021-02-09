8db696c09fa58f9df52fdeb2674e2eb84b92f9bb
Jan Janssen <jan-janssen@users.noreply.github.com>
2021-02-10 00:10:55 +0100

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -9,12 +9,8 @@ cp vcsgc-lammps/fix_semigrandcanonical_mc.* src/USER-VCSGC
 # Mlip
 if [[ -z "$MACOSX_DEPLOYMENT_TARGET" ]]; then
   args=$args" -D PKG_USER-MLIP=ON"
-  mkdir -p lib/mlip
-  cp ${PREFIX}/lib/libmlip.a lib/mlip
-  cp mlip/make/LAMMPS/Makefile.lammps.template lib/mlip/Makefile.lammps
+  export LDFLAGS="-L$PREFIX/lib -lmlip $LDFLAGS"
   cp -r mlip/src/external/MLIP4LAMMPS/USER-MLIP src/
-  cp mlip/make/LAMMPS/Install.sh src/USER-MLIP/
-  cp mlip/make/LAMMPS/README src/USER-MLIP/
 fi
 
 # Serial
