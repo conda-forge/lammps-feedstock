d226553237c423356ef1350329a83e1d3bc7522b
Sarath Menon <sarathmenon@mailbox.org>
2022-05-27 17:14:47 +0200

    update build

--- recipe/build.sh
+++ recipe/build.sh
@@ -42,9 +42,11 @@ cd build_lib
 cmake -D BUILD_LIB=ON -D BUILD_SHARED_LIBS=ON -D BUILD_MPI=ON -D PKG_MPIIO=ON -D LAMMPS_EXCEPTIONS=yes $args ../cmake
 make # -j${NUM_CPUS}
 cp liblammps${SHLIB_EXT}* ../src  # For compatibility with the original make system.
+cp libyaml-cpp-pace${SHLIB_EXT}* ../src
 cd ../src
 make install-python 
 mkdir -p $PREFIX/include/lammps
 cp library.h $PREFIX/include/lammps
 cp liblammps${SHLIB_EXT}* "${PREFIX}"/lib/
+cp libyaml-cpp-pace${SHLIB_EXT}* "${PREFIX}"/lib/
 cd ..
