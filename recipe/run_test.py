1a8d115b81aa56d7384f4a6e277fe5905b7cbaa1
Jan Janssen <jan-janssen@users.noreply.github.com>
2023-04-18 19:45:07 -0600

    Update run_test.py

--- recipe/run_test.py
+++ recipe/run_test.py
@@ -6,5 +6,9 @@ os.environ['OMPI_MCA_btl_vader_single_copy_mechanism'] = 'none'
 
 # Test
 from lammps import lammps
-
 lmp = lammps()
+
+# mliap test - currenty only working on linux
+import platform
+if platform.system() == "Linux":
+    from lammps.mliap import activate_mliappy
