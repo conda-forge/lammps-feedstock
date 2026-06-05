db301da241d21150f7aecc6904b44e6534cb7fb4
Jan Janssen <jan-janssen@users.noreply.github.com>
2026-06-05 21:41:16 +0200

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -57,7 +57,9 @@ if [[ -z "$MACOSX_DEPLOYMENT_TARGET" ]]; then
   args=$args" -D PKG_ML-HDNNP=ON -D DOWNLOAD_N2P2=OFF -D N2P2_DIR=${PREFIX} -D PKG_LATTE=ON"
   export LDFLAGS="-L$PREFIX/lib -lcblas -lblas -llapack -fopenmp $LDFLAGS"
   if [[ ${cuda_compiler_version} != "None" ]]; then
-    args=$args" -D PKG_KOKKOS=yes -D Kokkos_ENABLE_CUDA=yes ${Kokkos_OPT_ARGS}"
+    export NVCC_WRAPPER_SHOW_COMMANDS_BEING_RUN=1
+    export CXXFLAGS="$CXXFLAGS -I$BUILD_PREFIX/x86_64-conda-linux-gnu/sysroot/usr/include"
+    args=$args" -D PKG_KOKKOS=yes -D Kokkos_ENABLE_CUDA_CONSTEXPR=ON -D Kokkos_ENABLE_CUDA=yes ${Kokkos_OPT_ARGS}"
   fi
 else
   CXXFLAGS="${CXXFLAGS} -DTARGET_OS_OSX=1"
