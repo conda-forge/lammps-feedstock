d57d6e23393ca32e76d18fa990f065ffb2a6233c
Jan Janssen <jan-janssen@users.noreply.github.com>
2026-06-06 10:31:14 +0200

    Correct LDFLAGS and add CUDA-related CXXFLAGS
    Fix LDFLAGS export syntax and add CXXFLAGS for CUDA.

--- recipe/build.sh
+++ recipe/build.sh
@@ -55,9 +55,10 @@ args+=" -D PKG_ELECTRODE=yes"
 # Plugins - n2p2 and latte
 if [[ -z "$MACOSX_DEPLOYMENT_TARGET" ]]; then
   args=$args" -D PKG_ML-HDNNP=ON -D DOWNLOAD_N2P2=OFF -D N2P2_DIR=${PREFIX} -D PKG_LATTE=ON"
-  export LDFLAGS="-L$PREFIX/lib -lcblas -lblas -llapack -lm -fopenmp $LDFLAGS"
+  export LDFLAGS="-L$PREFIX/lib -lcblas -lblas -llapack =-fopenmp $LDFLAGS"
   if [[ ${cuda_compiler_version} != "None" ]]; then
     export NVCC_WRAPPER_SHOW_COMMANDS_BEING_RUN=1
+    export CXXFLAGS="$CXXFLAGS -D_GNU_SOURCE -D_DEFAULT_SOURCE -D_ISOC99_SOURCE"
     args=$args" -D PKG_KOKKOS=yes -D Kokkos_ENABLE_CUDA_CONSTEXPR=ON -D Kokkos_ENABLE_CUDA=yes ${Kokkos_OPT_ARGS}"
   fi
 else
