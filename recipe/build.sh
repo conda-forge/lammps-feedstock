6ce00b463316f10fc006d9fc8bd725fc9c681870
Jan Janssen <jan-janssen@users.noreply.github.com>
2026-06-06 22:24:32 +0200

    Set NVCC_PREPEND_FLAGS for CUDA include path
    Add NVCC_PREPEND_FLAGS to include directory for CUDA.

--- recipe/build.sh
+++ recipe/build.sh
@@ -58,6 +58,7 @@ if [[ -z "$MACOSX_DEPLOYMENT_TARGET" ]]; then
   export LDFLAGS="-L$PREFIX/lib -lcblas -lblas -llapack -fopenmp $LDFLAGS"
   if [[ ${cuda_compiler_version} != "None" ]]; then
     export NVCC_WRAPPER_SHOW_COMMANDS_BEING_RUN=1
+    export NVCC_PREPEND_FLAGS="-I $PREFIX/include"
     args=$args" -D PKG_KOKKOS=yes -D Kokkos_ENABLE_CUDA_CONSTEXPR=ON -D Kokkos_ENABLE_CUDA=yes ${Kokkos_OPT_ARGS}"
   fi
 else
